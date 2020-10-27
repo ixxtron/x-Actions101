eb5f19ddd6ff19d5efcbe40a6d17be58bc983c29
xTron Zamora <71669680+ixxtron@users.noreply.github.com>
2020-10-27 14:42:09 -0600

    Create main-proto1.tf

---
+++ main-proto1.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "ixproto1-terraform-state"
+    key    = "default-infrastructure"
+    region = "us-east-2"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "ixproto1-terraform-state"
+
+  versioning {
+    enabled = true
+  }
+}
